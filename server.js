93d5dbdf5233c5efbb354161289d79048837e123
William Miller <williamhmiller12@gmail.com>
2021-02-26 20:02:27 -0500

    Added Packages

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
